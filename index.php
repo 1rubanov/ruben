401b6333d210168e82c8f3e9ca9c58ffb8d3f0ce
Alexander <1rubanov.av@gmail.com>
2018-02-02 18:48:14 +0300

    Test

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
 
+<head>
+    <meta charset="UTF-8">
+    <title>HTML и CSS (базовый)</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
+<body>
+   
+    <?php
 
-?>
+        
+
+    ?>
+    
+    <script src="script.js"></script>
+</body>
+
+</html>
